d7bdf26919d568b93118d692e4cb658d11070285
Binh Nguyen <binh@endpointclosing.com>
2019-09-02 14:49:19 -0700

    Updated Link styles and docs

--- src/components/Link/Readme.md
+++ src/components/Link/Readme.md
@@ -1,7 +1,7 @@
 ### Start of Link component
 
 ```js
-<Link href='#link' color='primary' fontSize={2}>
+<Link href='#link' color='primary'>
   Link
 </Link>
 ```
--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -3,18 +3,22 @@ import { color, typography } from 'styled-system';
 import PropTypes from 'prop-types';
 
 const Link = styled.a`
+  /* ${color} */
   ${typography}
-  ${color}
 
   font-family: ${({ theme }) => theme.fonts.body};
   text-decoration: ${({ underline }) => (underline ? 'underline' : 'none')};
-  /* border: ${({ theme }) => `1px solid ${theme.colors.tertiary}`}; */
 
   /* Slack's accessibility outline feature */
   &:focus {
     /* incomplete dotted outline */
     outline: ${({ theme, color }) => `2px dotted ${theme.colors[color][1]}`};
     outline-offset: 5px;
+  }
+
+  /* Order matters, :visited must be above :hover and :active */
+  &:visited {
+    color: ${({ theme, color }) => theme.colors[color][1]};
   }
 
   &:hover {
@@ -25,16 +29,15 @@ const Link = styled.a`
     color: ${({ theme, color }) => theme.colors[color][1]};
 
   /* TODO: not working as expected */
-  &:visited {
-    color: none;
-  }
+
 }
 `;
 
 Link.defaultProps = {
   as: 'a',
   underline: false,
-  color: 'primary'
+  color: 'primary',
+  fontSize: 2
 };
 
 Link.propTypes = {
