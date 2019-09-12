51120b21dea36ae578acee0444dc4e3ba5b0a6a1
Binh Nguyen <binh@endpointclosing.com>
2019-09-11 20:09:18 -0700

    Added components to root file to export

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,26 @@
 import Box from './Box';
 import Button from './Button';
+import Card from './Card';
+import Divider from './Divider';
+import Flex from './Flex';
+import Grid from './Grid';
+import Layout from './Layout';
 import Link from './Link';
+import NavTab from './NavTab';
+import NavUnderline from './NavUnderline';
 import Text from './Text';
 
 const components = {
   Box,
   Button,
+  Card,
+  Divider,
+  Flex,
+  Grid,
+  Layout,
   Link,
+  NavTab,
+  NavUnderline,
   Text
 };
 
